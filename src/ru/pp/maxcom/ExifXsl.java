3e8d07c3f330978ca5124ab6cd072462fb7c439f
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-10-27 18:51:55 +0400

    update metadata extractor, remove lnxorgutil

--- src/ru/pp/maxcom/ExifXsl.java
+++ src/ru/pp/maxcom/ExifXsl.java
@@ -1,60 +1,57 @@
 package ru.pp.maxcom;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Document;
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Attr;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-import java.util.Calendar;
-
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Directory;
-import com.drew.metadata.MetadataException;
-import com.drew.metadata.iptc.IptcDirectory;
-import com.drew.metadata.exif.ExifDirectory;
-import com.drew.metadata.exif.CanonMakernoteDirectory;
-import com.drew.metadata.exif.CanonMakernoteDescriptor;
-import com.drew.metadata.exif.GpsDirectory;
 import com.drew.imaging.jpeg.JpegMetadataReader;
 import com.drew.imaging.jpeg.JpegProcessingException;
 import com.drew.lang.Rational;
-import ru.org.linux.util.ImageInfo;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDirectory;
+import com.drew.metadata.iptc.IptcDirectory;
+import com.drew.metadata.jpeg.JpegDirectory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import ru.org.linux.util.BadImageException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class ExifXsl {
   public static final String EXIF_NS = "http://maxcom.pp.ru/photo/exif";
   final Directory exifDirectory;
-  final ImageInfo info;
   private final Directory gpsDirectory;
   private final Directory iptcDirectory;
+  private ExifIFD0Directory exifIfd0Directory;
+  private JpegDirectory jpegDirectory;
 
   public ExifXsl(String filename) throws JpegProcessingException, IOException, BadImageException {
+    // System.out.println(filename);
+
     File jpegFile = new File(filename);
     Metadata metadata = JpegMetadataReader.readMetadata(jpegFile);
-    exifDirectory = metadata.getDirectory(ExifDirectory.class);
+    exifDirectory = metadata.getDirectory(ExifSubIFDDirectory.class);
+    exifIfd0Directory = metadata.getDirectory(ExifIFD0Directory.class);
     gpsDirectory = metadata.getDirectory(GpsDirectory.class);
     iptcDirectory = metadata.getDirectory(IptcDirectory.class);
-
-    info = new ImageInfo(filename);
+    jpegDirectory = metadata.getDirectory(JpegDirectory.class);
   }
 
   public Element getExif() throws  ParserConfigurationException, MetadataException {
     Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().getDOMImplementation().createDocument(EXIF_NS, "exif", null);
 
     Element camera = document.createElement("camera");
-    camera.appendChild(document.createTextNode(exifDirectory.getString(ExifDirectory.TAG_MODEL)));
+    camera.appendChild(document.createTextNode(exifIfd0Directory.getString(ExifIFD0Directory.TAG_MODEL)));
     document.getDocumentElement().appendChild(camera);
 
     Element exposure = document.createElement("exposure");
-    exposure.setAttribute("aperture", exifDirectory.getString(ExifDirectory.TAG_FNUMBER));
-    exposure.setAttribute("speed", exifDirectory.getString(ExifDirectory.TAG_EXPOSURE_TIME));
-    exposure.setAttribute("iso", exifDirectory.getString(ExifDirectory.TAG_ISO_EQUIVALENT));
+    exposure.setAttribute("aperture", exifDirectory.getString(ExifSubIFDDirectory.TAG_FNUMBER));
+    exposure.setAttribute("speed", exifDirectory.getString(ExifSubIFDDirectory.TAG_EXPOSURE_TIME));
+    exposure.setAttribute("iso", exifDirectory.getString(ExifSubIFDDirectory.TAG_ISO_EQUIVALENT));
 
 /*    int mode = exifDirectory.getInt(ExifDirectory.TAG_EXPOSURE_PROGRAM);
     switch (mode) {
@@ -73,15 +70,17 @@ public class ExifXsl {
       default:
     }
 */
-    int flashMode = exifDirectory.getInt(ExifDirectory.TAG_FLASH);
-    if ((flashMode & 1)==1) {
-      exposure.setAttribute("flash", "TRUE");
+    if (exifDirectory.containsTag(ExifSubIFDDirectory.TAG_FLASH)) {
+      int flashMode = exifDirectory.getInt(ExifSubIFDDirectory.TAG_FLASH);
+      if ((flashMode & 1) == 1) {
+        exposure.setAttribute("flash", "TRUE");
+      }
     }
 
     document.getDocumentElement().appendChild(exposure);
 
     Element date = document.createElement("date");
-    Date originalDate = exifDirectory.getDate(ExifDirectory.TAG_DATETIME_ORIGINAL);
+    Date originalDate = exifDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
     date.appendChild(document.createTextNode(originalDate.toString()));
 
     long age = (System.currentTimeMillis()-originalDate.getTime())/(1000*60*60*24);
@@ -90,7 +89,7 @@ public class ExifXsl {
     document.getDocumentElement().appendChild(date);
 
     Element lens = document.createElement("lens");
-    lens.appendChild(document.createTextNode(exifDirectory.getString(ExifDirectory.TAG_FOCAL_LENGTH)));
+    lens.appendChild(document.createTextNode(exifDirectory.getString(ExifSubIFDDirectory.TAG_FOCAL_LENGTH)));
     document.getDocumentElement().appendChild(lens);
 
 /*    Element distance = document.createElement("distance");
@@ -128,11 +127,11 @@ public class ExifXsl {
     return document.getDocumentElement();
   }
 
-  public int getWidth() {
-    return info.getWidth();
+  public int getWidth() throws MetadataException {
+    return jpegDirectory.getInt(JpegDirectory.TAG_JPEG_IMAGE_WIDTH);
   }
 
-  public int getHeight() {
-    return info.getHeight();
+  public int getHeight() throws MetadataException {
+    return jpegDirectory.getInt(JpegDirectory.TAG_JPEG_IMAGE_HEIGHT);
   }
 }
